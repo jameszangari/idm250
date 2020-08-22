5ed440edb9d76883a7a87e0a81ac67e1614796e1
James Cliff <me@jamescliff.com>
2020-08-22 17:21:58 -0400

    :bug: disabled scroll on body when hamburger menu is open

--- public/wp-content/themes/untitledrecordings/dist/scripts/scripts.js
+++ public/wp-content/themes/untitledrecordings/dist/scripts/scripts.js
@@ -3,6 +3,7 @@ var $ = jQuery.noConflict();
 $(document).ready(function(){
     $("button").click(function(){
       $(".menu-wrapper").toggle();
+      $("body").toggleClass("hidden");   
     });
 
     $(".js-fillcolor").fillColor({type: 'avgYUV'});
