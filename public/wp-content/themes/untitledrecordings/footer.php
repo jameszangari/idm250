42f0ac389ae8213440aee8be4057e78f5549edc6
James Cliff <me@jamescliff.com>
2020-08-22 16:12:00 -0400

    :bento: added mail button to footer menu

--- public/wp-content/themes/untitledrecordings/footer.php
+++ public/wp-content/themes/untitledrecordings/footer.php
@@ -1,6 +1,7 @@
 <footer>
     <div class="footer">
         <div class="footer-menu-icons">
+            <a href="mailto:inquiries@untitledrecordings.net"><i class="fas fa-envelope"></i></a>
             <a href="https://instagram.com/untitledrecordings"><i class="fab fa-instagram"></i></a>
             <a href="https://twitter.com/untrecordings"><i class="fab fa-twitter"></i></a>
             <a href="https://discord.gg/G5V8QG9"><i class="fab fa-discord"></i></a>
