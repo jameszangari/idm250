326c8f27906a98d6ed5ae864513e48d56d0f877f
James Cliff <me@jamescliff.com>
2020-08-22 17:04:05 -0400

    :bug: socials in header/footer open in new tab :lock: with security fixes

--- public/wp-content/themes/untitledrecordings/header.php
+++ public/wp-content/themes/untitledrecordings/header.php
@@ -25,10 +25,10 @@
     </div>
     <?php wp_nav_menu(['theme_location' => 'primary_menu']);?>
       <div class="menu-wrapper-icons">
-        <a href="mailto:inquiries@untitledrecordings.net"><i class="fas fa-envelope"></i></a>
-        <a href="https://instagram.com/untitledrecordings"><i class="fab fa-instagram"></i></a>
-        <a href="https://twitter.com/untrecordings"><i class="fab fa-twitter"></i></a>
-        <a href="https://discord.gg/G5V8QG9"><i class="fab fa-discord"></i></a>
+        <a href="mailto:inquiries@untitledrecordings.net" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i></a>
+        <a href="https://instagram.com/untitledrecordings" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+        <a href="https://twitter.com/untrecordings" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
+        <a href="https://discord.gg/G5V8QG9" target="_blank" rel="noopener noreferrer"><i class="fab fa-discord"></i></a>
       </div>
     </div>
   </div>
