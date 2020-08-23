13615212b964cfd946ba1f25913fc39096371c37
James Cliff <me@jamescliff.com>
2020-08-23 10:32:15 -0400

    :bug: fixed header/footer target attribute

--- public/wp-content/themes/untitledrecordings/footer.php
+++ public/wp-content/themes/untitledrecordings/footer.php
@@ -3,8 +3,8 @@
         <div class="footer-menu-icons">
             <a href="<?php echo get_field('Email', 'option')["url"]; ?>" target="<?php echo get_field('Email', 'option')["target"]; ?>"><i class="fas fa-envelope"></i></a>
             <a href="<?php echo get_field('Instagram', 'option')["url"]; ?>" target="<?php echo get_field('Instagram', 'option')["target"]; ?>"><i class="fab fa-instagram"></i></a>
-            <a href="<?php echo get_field('Twitter', 'option')["url"]; ?>" target="<?php echo get_field('Twitter', 'option')["url"]; ?>"><i class="fab fa-twitter"></i></a>
-            <a href="<?php echo get_field('Discord', 'option')["url"]; ?>" target="<?php echo get_field('Discord', 'option')["url"]; ?>"><i class="fab fa-discord"></i></a>
+            <a href="<?php echo get_field('Twitter', 'option')["url"]; ?>" target="<?php echo get_field('Twitter', 'option')["target"]; ?>"><i class="fab fa-twitter"></i></a>
+            <a href="<?php echo get_field('Discord', 'option')["url"]; ?>" target="<?php echo get_field('Discord', 'option')["target"]; ?>"><i class="fab fa-discord"></i></a>
         </div>
         <a href="<?php echo get_site_url(); ?>" class="footer-logo"><img src="<?php echo get_field('logo-full', 'option')["url"]; ?>" alt="<?php echo get_field('logo-full', 'option')["alt"]; ?>"></a>
         <p>© 2020 Untitled Recordings | Website by <a href="https://jamescliff.com" class="footer-link" target="_blank" rel="noopener noreferrer">James Cliff</a></p>
