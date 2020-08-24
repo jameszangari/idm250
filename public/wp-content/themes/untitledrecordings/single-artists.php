0979a4a584b80952e60a55f635a6899c8b2e8220
James Cliff <me@jamescliff.com>
2020-08-23 23:34:16 -0400

    :sparkles: updated post type with dynamic tags, new socials, backup featured image, about section, and WIP portfolio

--- public/wp-content/themes/untitledrecordings/single-artists.php
+++ public/wp-content/themes/untitledrecordings/single-artists.php
@@ -5,46 +5,119 @@
     <?php while (have_posts()) : the_post(); ?>
     <h1 class="artist-title"><?php the_title(); ?></h1>
 
-    <?php $roles = get_field('roles'); ?>
-    <?php if ($roles) : ?>
-        <p class="artist-roles-title"><?php echo $roles ?></p>
-    <?php endif; ?>
+    <p class="artist-roles-title">
+    <?php
+        $id = get_the_ID();
+        $artist_roles = get_the_terms( $id, 'artist_roles' );
+        $total = count($artist_roles);
+    ?>
+    <?php if ($artist_roles) : ?>
+        <?php foreach ($artist_roles as $key => $roles){ ?>
+            <?php
+            // Get sub field values.
+            $name = $roles->name;
+            $link = get_term_link($roles->term_id);
+            ?>  
+        <a href="<?php echo $link; ?>">    
+          <?php echo $key + 1 != $total ? $roles->name . ', ' : $roles->name  ;?>
+        </a>  
+        <?php } ?>
+    <?php endif; ?>  
+    </p>
 
-    <?php if( have_rows('socials') ): ?>
-        <?php while( have_rows('socials') ): the_row(); 
-
+    <?php 
         // Get sub field values.
-        $instagram = get_sub_field('instagram');
-        $twitter = get_sub_field('twitter');
-        $soundcloud = get_sub_field('soundcloud');
-        $email = get_sub_field('email');
+        $instagram = get_field('instagram');
+        $twitter = get_field('twitter');
+        $soundcloud = get_field('soundcloud');
+        $email = get_field('email');
         //var_dump($email);
     ?>
-
-    <?php if ($instagram) : ?>
         <div class="artist-social-media">
-            <a href="<?php echo $instagram["url"] ?>" target="<?php echo $instagram["target"] ?>" class="artist-socials-instagram"><//?php echo $instagram["title"] ?><i class="fab fa-instagram"></i></a>
-            <a href="<?php echo $twitter["url"] ?>" target="<?php echo $twitter["target"] ?>" class="artist-socials-twitter"><//?php echo $twitter["title"] ?><i class="fab fa-twitter"></i></a>
-            <a href="<?php echo $soundcloud["url"] ?>" target="<?php echo $soundcloud["target"] ?>" class="artist-socials-soundcloud"><//?php echo $soundcloud["title"] ?><i class="fab fa-soundcloud"></i></a>
-            <a href="mailto:<?php echo $email ?>" target="_blank noopener noreferrer" class="artist-socials-email"><//?php echo $email["title"] ?><i class="fas fa-envelope"></i></a>
+          <?php if ($instagram) : ?>
+            <a href="<?php echo $instagram["url"] ?>" target="<?php echo $instagram["target"] ?>" class="artist-socials-instagram"><i class="fab fa-instagram"></i></a>
+          <?php endif; ?>
+          <?php if ($twitter) : ?>
+            <a href="<?php echo $twitter["url"] ?>" target="<?php echo $twitter["target"] ?>" class="artist-socials-twitter"><i class="fab fa-twitter"></i></a>
+          <?php endif; ?>
+          <?php if ($soundcloud) : ?>
+            <a href="<?php echo $soundcloud["url"] ?>" target="<?php echo $soundcloud["target"] ?>" class="artist-socials-soundcloud"><i class="fab fa-soundcloud"></i></a>
+          <?php endif; ?>
         </div>
-    <?php endif; ?>
-
-    <?php endwhile; ?>
-  <?php endif; ?>
+        <?php if ($email) : ?>
+            <a href="mailto:<?php echo $email ?>" target="_blank noopener noreferrer" class="artist-socials-email-link"><!-- <i class="fas fa-envelope"></i> -->
+              <p><?php echo $email ;?></p>
+            </a>
+          <?php endif; ?>
   </div>
 
   <div class="artist-intro-content-right">
-        <div class="artist-profile-image-shadow"></div>
-        <?php the_post_thumbnail( 'large', array( 'class' => 'artist-profile-image' ) );?>
+        <div class="artist-profile-image-shadow js-fillcolor">
+        <?php if ( has_post_thumbnail() ) {
+        the_post_thumbnail( 'large', array( 'class' => 'artist-profile-image' ) );
+      } else { ?>
+        <img src="<?php bloginfo('template_directory'); ?>/dist/images/image_not_set.jpg" alt="<?php the_title(); ?>" class="artist-profile-image"/>
+      <?php } ?>
+        </div>
   </div>
 </div>
 
-    <div class="container">
-      <div class="page-builder">
-        <?php the_content(); ?>
+  <?php 
+    // Get sub field values.
+    $bio = get_field('bio');
+  ?>
+  <?php if($bio) : ?>
+  <div class="single-artists-about">
+    <h2>About</h2>
+    <img src="<?php bloginfo('template_directory'); ?>/dist/images/waveform_white.png" alt="Waveform" class="heading-waveform-image">
+      <p><?php echo $bio; ?></p> 
+  </div>
+  <?php endif; ?>
+
+  <div class="portfolio">
+    <h2>Portfolio</h2>
+    <img src="<?php bloginfo('template_directory'); ?>/dist/images/waveform_white.png" alt="Waveform" class="heading-waveform-image">
+
+
+  <?php 
+  $args = array(
+    'numberposts' => 12,
+    'post_type'   => 'music'
+  );
+  
+  $latest_music = get_posts( $args );
+  //var_dump($latest_music);
+  ?>
+
+  <div class="portfolio-grid">
+  <?php if($latest_music) : ?>
+    <?php foreach ($latest_music as $music) :?>
+      <div class="portfolio-grid-image">
+        <img src="<?php echo get_the_post_thumbnail_url($music->ID); ?>" alt="<?php echo $music->post_title; ?>">
+        <div class="portfolio-grid-hover">
+          <h3><?php echo $music->post_title; ?></h3>
+            <a href="<?php echo get_permalink($music->ID); ?>" class="portfolio-hover-button-link">
+              <div class="portfolio-hover-button">
+                  <p>View Release</p>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="#F7F7FF" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>               
+              </div>
+            </a>         
+        </div>
       </div>
-    </div>
+    <?php endforeach; ?>
+  <?php endif; ?>
+  </div>
+  <a href="<?php echo get_post_type_archive_link('music'); ?>" class="portfolio-button-link">
+      <div class="portfolio-button">
+          <p>View All Releases</p>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="#F7F7FF" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>               
+      </div>
+    </a>     
+  </div>
+
+  <!-- <div class="single-artists-portfolio">
+  </div> -->
+
   <?php endwhile; ?>
 </main>
 <?php get_footer(); ?>
