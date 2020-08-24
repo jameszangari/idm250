e96d7841aeb19e5f2afb12d7754a388f8c79ef0e
James Cliff <me@jamescliff.com>
2020-08-23 23:24:19 -0400

    :heavy_minus_sign: removed portfolio post type :bug: enabled archive for other cpts for menus

--- public/wp-content/themes/untitledrecordings/lib/custom-post-types.php
+++ public/wp-content/themes/untitledrecordings/lib/custom-post-types.php
@@ -56,7 +56,7 @@ function register_projects_post_type() {
     'show_in_admin_bar'     => true,
     'show_in_nav_menus'     => true,
     'can_export'            => true,
-    'has_archive'           => false,
+    'has_archive'           => true,
     'exclude_from_search'   => false,
     'publicly_queryable'    => true,
     'show_in_rest'          => true,
@@ -66,65 +66,6 @@ function register_projects_post_type() {
 ];
 
     register_post_type('music', $args);
-
-    // WORK
-    $singular = 'Portfolio';
-    $plural = 'Portfolio';
-
-    $labels = [
-    'name'                  => $plural,
-    'singular_name'         => $singular,
-    'archives'              => '' . $singular . ' Archives',
-    'attributes'            => '' . $singular . ' Attributes',
-    'all_items'             => 'All ' . $plural . '',
-    'add_new_item'          => 'Add New ' . $plural . '',
-    'add_new'               => 'Add New',
-    'featured_image'        => 'Featured Image',
-    'set_featured_image'    => 'Set featured image',
-    'remove_featured_image' => 'Remove featured image',
-    'use_featured_image'    => 'Use as featured image',
-    'insert_into_item'      => 'Insert into ' . $singular . '',
-    'uploaded_to_this_item' => 'Uploaded to this ' . $singular . '',
-    'items_list'            => '' . $plural . ' list',
-    'items_list_navigation' => '' . $plural . ' list navigation',
-    'filter_items_list'     => 'Filter ' . $singular . ' list',
-    ];
-
-    $args = [
-    'label'                 => $plural,
-    'labels'                => $labels,
-    'supports'              => [
-        'title',
-        'editor',
-        'author',
-        'thumbnail',
-        'excerpt',
-        'trackbacks',
-        'custom-fields',
-        'comments',
-        'revisions',
-        'page-attributes',
-        'post-formats'
-    ],
-    'taxonomies'            => [],
-    'hierarchical'          => false,
-    'public'                => true,
-    'show_ui'               => true,
-    'show_in_menu'          => true,
-    'menu_position'         => 5,
-    'show_in_admin_bar'     => true,
-    'show_in_nav_menus'     => true,
-    'can_export'            => true,
-    'has_archive'           => false,
-    'exclude_from_search'   => false,
-    'publicly_queryable'    => true,
-    'show_in_rest'          => true,
-    // Dash Icons https://developer.wordpress.org/resource/dashicons/#media-audio
-    'menu_icon'             => 'dashicons-playlist-audio'
-    // 'menu_icon'             => get_stylesheet_directory_uri() . '/static/images/icons/industries.png'
-];
-
-    register_post_type('portfolio', $args);
 
     // ARTISTS
     $singular = 'Artist';
@@ -174,7 +115,7 @@ function register_projects_post_type() {
     'show_in_admin_bar'     => true,
     'show_in_nav_menus'     => true,
     'can_export'            => true,
-    'has_archive'           => false,
+    'has_archive'           => true,
     'exclude_from_search'   => false,
     'publicly_queryable'    => true,
     'show_in_rest'          => true,
