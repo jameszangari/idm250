b5b19d148bd81fde273294ad8c1a828a7c479212
James Cliff <me@jamescliff.com>
2020-08-31 10:09:23 -0400

    :sparkles: added template for pages

---
+++ public/wp-content/themes/untitledrecordings/page.php
@@ -0,0 +1,21 @@
+<?php get_header();?>
+<main class="main-content">
+<div class="archive-hero">
+  <a href="<?php echo get_site_url(); ?>">
+    <img src="<?php echo get_field('logo-full', 'option')["url"]; ?>" alt="<?php echo get_field('logo-full', 'option')["alt"]; ?>" class="archive-hero-image">
+  </a>
+</div>
+  <?php while (have_posts()) : the_post(); ?>
+    <div class="container-contact">
+        <!-- Main Content -->
+        <div class="column">
+          <h1 class="post_title"><?php the_title(); ?></h1>
+          <?php the_post_thumbnail(); ?>
+          <div class="page-builder">
+            <?php the_content(); ?>
+          </div>
+        </div>
+    </div>
+  <?php endwhile; ?>
+</main>
+<?php get_footer(); ?>
