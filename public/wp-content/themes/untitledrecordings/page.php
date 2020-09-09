b610a3ca99e09b8f888968a59ae12db7daa051fd
James Cliff <me@jamescliff.com>
2020-09-09 13:03:14 -0400

    :bug: included page excerpt for page templates

--- public/wp-content/themes/untitledrecordings/page.php
+++ public/wp-content/themes/untitledrecordings/page.php
@@ -10,6 +10,7 @@
         <!-- Main Content -->
         <div class="column">
           <h1 class="post_title"><?php the_title(); ?></h1>
+          <?php the_excerpt(); ?>
           <?php the_post_thumbnail(); ?>
           <div class="page-builder">
             <?php the_content(); ?>
